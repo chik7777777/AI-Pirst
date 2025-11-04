2127f791f523625de683fb4ba223466e25b88fae
chik7777777 <chick090429@gmail.com>
2025-11-04 13:54:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.title('안녕하십니까 이곳은 저의 처음입니다.^v^')
 name=st.text_input('이름을 입력해주세요.^ㅇ^')
 if st.button('인사말 생성'):
-  st.wirte(name+'님! 하요!!>v<')
+  st.write(name+'님! 하요!!>v<')
   
