7d701a56a8ff8cdef1313652852df2cd53af46f3
chik7777777 <chick090429@gmail.com>
2025-11-04 14:05:19 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 st.title('안녕하십니까 이곳은 저의 처음입니다.^v^')
 name=st.text_input('이름을 입력해주세요.^ㅇ^')
-st.selectbox('좋아하는 음식을 선택해주세요.^o^',['치킨','떡볶이','규카츠','찜닭'])
+m=st.selectbox('좋아하는 음식을 선택해주세요.^o^',['치킨','떡볶이','규카츠','찜닭'])
 if st.button('인사말 생성'):
-  st.write(name+'님 하요!!!>v<')
+  st.info(name+'님 하요!!!>v<')
+  st.warning(m+'을 좋아하시는군요. 저도 좋아해요!>_<')
+  st.error('반갑습니당~!^~^')
